d4c21dc9387e77be48ad1706ab82f5ab3a2bccfb
Mamoudou Sacko <msacko99@yahoo.com>
2020-01-09 19:05:01 -0500

    add client file

--- TP1/src/Client.java
+++ TP1/src/Client.java
@@ -1,4 +1,21 @@
-
+import java.io.DataInputStream;
+import java.net.Socket;
 public class Client {
+	
+	private static Socket socket;
+	
+	public static void main(String[] args) throws Exception
+	{
+		String serverAddress = "127.0.0.1";
+		int port = 5000;
+		
+		socket = new Socket(serverAddress, port);
+		
+		DataInputStream in = new DataInputStream(socket.getInputStream());
+		
+		String helloMessageFromServer = in.readUTF();
+		
+		socket.close();
+	}
 
 }
